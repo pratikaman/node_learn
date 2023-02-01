f86432ed47dc2cf2d05c85bbf215a8bf85b29ef7
pratikaman <pratikaman.atom@gmail.com>
2023-02-01 19:52:45 +0530

    completed till streams

---
+++ 16_read_stream.js
@@ -0,0 +1,17 @@
+const {createReadStream} = require('fs')
+
+// default 64kb
+// last buffer - remainder
+// highWaterMark - control size
+// const stream = createReadStream('./content/big.txt', { highWaterMark: 90000 })
+// const stream = createReadStream('../content/big.txt', { encoding: 'utf8' })
+
+const stream = createReadStream('./content/big.txt', 'utf8');
+
+stream.on("data", (data)=>{
+    console.log(data);
+})
+
+stream.on('error', (err)=>{
+    console.log(err);
+})
